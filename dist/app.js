b2076c00525fe423b20ffe83b6ed0ba06563597f
Florian LUCAS <florian@mcube.fr>
2022-02-26 15:58:43 +0100

    added all platforms favicon

--- dist/app.js
+++ dist/app.js
@@ -925,32 +925,6 @@ class FlagNerdHard {
 	}
 }
 
-class GeoNerdNavigation {
-	constructor() {
-		this.pages = document.querySelectorAll(".pages .page");
-
-		this.changePage();
-
-		window.addEventListener("hashchange", e => {
-			this.changePage();
-		});
-	}
-
-	changePage() {
-		let navTo = location.hash;
-		if (!navTo){
-			navTo = "#home";
-		}
-		const nextPage = document.querySelector(navTo);
-		if (nextPage) {
-			this.pages.forEach(page => {
-				page.classList.remove("active");
-			});
-			nextPage.classList.add("active");
-		}
-	}
-}
-
 class HomeCards {
 	constructor() {
 		this.cards = document.querySelectorAll(".game-card.has-levels");
@@ -1024,6 +998,32 @@ class Settings {
 			localStorage.removeItem("capitalnerdhard.lifes");
 			e.target.classList.add("done");
 		});
+	}
+}
+
+class GeoNerdNavigation {
+	constructor() {
+		this.pages = document.querySelectorAll(".pages .page");
+
+		this.changePage();
+
+		window.addEventListener("hashchange", e => {
+			this.changePage();
+		});
+	}
+
+	changePage() {
+		let navTo = location.hash;
+		if (!navTo){
+			navTo = "#home";
+		}
+		const nextPage = document.querySelector(navTo);
+		if (nextPage) {
+			this.pages.forEach(page => {
+				page.classList.remove("active");
+			});
+			nextPage.classList.add("active");
+		}
 	}
 }
 
